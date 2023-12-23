64b4365a60e7e681c2d0d63be442abc4ef126930
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2023-12-23 23:58:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    "success": true,
+    "message": "Hello, world!"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
